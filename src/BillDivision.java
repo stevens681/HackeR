ac20b1ae388d0b0b792a76c168f8b1e4645f7c68
fernandorosa <stevensfrr@gmail.com>
2022-05-31 18:10:06 -0400

    Laptop

---
+++ src/BillDivision.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class BillDivision {
+    /*
+     * Complete the 'bonAppetit' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY bill
+     *  2. INTEGER k
+     *  3. INTEGER b
+     */
+    public static void bonAppetit(List<Integer> bill, int k, int b) {
+        // Write your code here
+        int sum=0;
+        for(int i=0; i<bill.size(); i++){
+            if(k==i)continue;
+            sum+=bill.get(i);
+        }
+        double split = sum/2;
+        if(b==Math.round(split))
+            System.out.println("Bon Appetit");
+        else
+            System.out.println(b-Math.round(split));
+    }
+
+
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>();
+        Random n=new Random();
+        for(int i=0; i<4; i++){
+            arr.add(n.nextInt(100));
+        }
+
+        BillDivision.bonAppetit(arr, n.nextInt(100),n.nextInt(100));
+
+    }
+}
