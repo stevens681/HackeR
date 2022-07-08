403dc4cbb00ab83f34c7db9e8c5146774e0095f0
fernandorosa <stevensfrr@gmail.com>
2022-07-08 12:51:08 -0400

    Laptop

--- src/NewYearChaos.java
+++ src/NewYearChaos.java
@@ -10,11 +10,30 @@ public class NewYearChaos {
 
     public static void minimumBribes(List<Integer> q) {
         // Write your code here
-        System.out.println(q);
+        int res=0;
+        String bribe ="";
+        for(int i = q.size()-1; i>=0; i--){
+
+            if(q.get(i)-(i+1) > 2){
+                bribe ="Too chaotic";
+                break;
+            }
+            for(int j = Math.max(0, q.get(i)-2); j<i; j++){
+                if(q.get(j)>q.get(i)){
+                    res++;
+                }
+
+            }
+            bribe = Integer.toString(res);
+
+        }
+        System.out.println(bribe);
     }
 
     public static void main(String[] args) {
-        String list = "2 1 5 3 4";
+//        String list = "2 1 5 3 4";
+        String list = "2 5 1 3 4";
+
         List<Integer> arr = new ArrayList<>();
 
         char[] a= list.toCharArray();
